0480bcda6c15bbabbb1a15985d4352f8b9d8dca5
NaCl <105908496+skp07111@users.noreply.github.com>
2022-11-09 23:28:45 +0900

    [제출]파일 read 과제

---
+++ Assignment5/src/Manager.java
@@ -0,0 +1,253 @@
+import java.io.*;
+
+public class Manager {
+	private Product[]productList; // 상품 배열
+	private int productCount = 0; // 상품 배열 인덱스 카운트
+	private User[]userList; // 대여 배열
+	private int userCount = 0; // 대여 배열 인덱스 카운트
+	private int revenue = 0; // 일일 매출 총액 변수
+	FileInputStream fis = null;
+	DataInputStream dis = null;
+	
+	// 생성자 (상품 배열, 대여 배열 크기 설정)
+	Manager (int maxProductCount, int maxUserCount) throws Exception {
+		productList = new Product[maxProductCount]; // 상품 배열 크기 설정
+		userList = new User[maxUserCount]; // 대여 배열 크기 설정
+		/* 파일 정보 reader */
+		try {
+			fis = new FileInputStream("rental.dat"); // FileInputStream 객체 fis 생성
+			dis = new DataInputStream(fis); // DataInputStream 객체 dis 생성
+		} 
+		catch(Exception e) {
+			throw new Exception ("파일을 찾을 수 없습니다.");
+		}	
+	}
+	
+
+	// 코드 중복 검색
+	public void checkCode(Product p) throws Exception {
+		for (int i = 0; i < productCount; i++)
+		{
+			Product p1 = productList[i];
+			// 중복된 키 검색
+			if(p1 != null && p1.getCode().equals(p.getCode()))
+			{
+				throw new Exception ("잘못된 상품 등록입니다.");
+			}
+		}
+	}
+
+	// 상품 배열에 원소 추가
+	public void addProduct(Product p) {
+		productList[productCount] = p;
+		productCount++;
+	}
+	
+	// 상품 추가
+	public void add(Product p) throws Exception {
+		try{
+			checkCode(p); // 코드 중복 검색
+			addProduct(p); // 상품 추가
+		}
+		catch (Exception e) {
+			throw new Exception ("잘못된 상품 등록입니다.");
+		}
+	}
+	
+	// 상품 배열에 number번째 인덱스 원소 삭제
+	public void subProduct(int number) {
+		// 상품 삭제, 배열 정리하기	
+		for (int i = number; i < productCount; i++)	
+		{		
+			productList[i] = productList[i+1];		
+		}
+		productCount--;
+	}
+	
+	// 상품 삭제
+	public void delete(String productCode) throws Exception {
+		try{
+			int number = search(productCode); // 상품 배열에서 검색하기
+			subProduct(number); // 상품 배열에서 삭제하기
+		}
+		catch (Exception e) {
+			throw new Exception ("존재하지 않는 상품입니다.");
+		}
+	}
+	
+	// 상품 객체 검색
+	public int search(String productCode) throws Exception {	
+		// 상품 검색
+		for(int i = 0; i < productCount; i++)
+		{
+			Product p1 = productList[i]; // 객체 생성
+			// 원소 코드와 인수 일치
+			if((productCode.equals(p1.getCode())))
+			{
+				return i;
+			}
+		} throw new Exception("일치하는 코드를 찾을 수 없습니다."); // 코드가 일치하지 않으면 익셉션 발생
+	}
+
+	// 상품 배열 i번째 리턴
+	public Product productAt(int i) {
+		return productList[i]; // 상품 배열 i번째 상품 객체 return
+	}
+	
+	// productCount 값 반환
+	public int getProductCount() {
+		return productCount;
+	}
+	
+	// 전화번호 중복 검색
+	public void checkPhone(User u) throws Exception {
+		for (int i = 0; i < userCount; i++)
+		{
+			User u1 = userList[i];
+			// 중복된 전화번호 검색
+			if(u1 != null && u1.getPhone().equals(u.getPhone()))
+			{
+				throw new Exception ("잘못된 방법의 체크인입니다."); // 중복된 전화번호일 경우 익셉션 발생
+			}
+		}
+	}
+	
+	// 재고 개수에서 대여 개수 제외
+	public void subStock(User u) throws Exception {
+		// 재고 개수에서 대여 개수 제외하기
+		for(int i = 0; i < u.getRentalCount(); i++)
+		{
+			String code = u.codeAt(i); // 해당 User 객체의 i번째 대여 물품 코드
+			int searchNum;
+			try {
+				searchNum = search(code); // productList에서 해당 코드의 인덱스 번호 검색
+			} 
+			catch (Exception e) {
+				throw new Exception("잘못된 방법의 체크인입니다.");
+			}
+			Product p = productList[searchNum]; //해당 인덱스의 product 객체
+			p.subNumber(); // 대여가 가능한지 확인 후 빌리기 (재고 1개 삭제)
+		}
+	}
+	
+	// 대여 배열에 원소 추가
+	public void addUser(User u) {
+		userList[userCount] = u;
+		userCount++;
+	}
+	
+	// 체크인
+	public void checkIn(User u) throws Exception {
+		try {
+			checkPhone(u); // 전화번호 중복 검색
+			subStock(u); // 재고 개수에서 대여 개수 제외
+			addUser(u); // 대여 배열에 대여 정보 넣기
+		}
+		catch(Exception e) {
+			 throw new Exception("잘못된 방법의 체크인입니다.");
+		}
+	}
+	
+	/* 체크인 정보 넣는 함수 */
+	public void readCheckIn(User u) {
+		addUser(u); // 대여 배열에 대여 정보 넣기
+	}
+	
+	// userCount 값 반환
+	public int getUserCount() {
+		return userCount;
+	}
+	
+	// 대여 배열 i번째 리턴
+	public User userAt(int i)
+	{
+		return userList[i]; // 대여 배열 i번째 유저 객체 return
+	}
+	
+	// 일치하는 회원번호 검색
+	public int searchUser(String phone) throws Exception {
+		for(int i = 0; i < userCount; i++)
+		{
+			User u = userList[i];
+			// 일치하는 정보가 있으면 인덱스 번호 반환
+			if (u.getPhone().equals(phone))
+				return i;
+		}throw new Exception ("회원정보가 없습니다."); // 일치하는 정보가 없으면 익셉션 발생
+	}
+	
+	// 상품 재고 다시 추가
+	public void addStock(int index) throws Exception {
+		User u = userAt(index);
+		for(int i = 0; i < u.getRentalCount(); i++) {
+			String code = u.codeAt(i); // 해당 User 객체의 i번째 대여 물품 코드
+			try {
+				int number = search(code); // productList에서 해당 코드의 인덱스 번호 검색
+				productAt(number).addNumber(); //해당 인덱스의 product 객체의 재고 추가하기
+			}
+			catch (Exception e) {
+				throw new Exception ("잘못된 방법의 체크아웃입니다.");
+			}
+		}
+	}
+	
+	// 대여 배열에 원소 삭제
+	public void subUser(int number) {
+		for (int i = number; i < userCount; i++)	
+		{		
+			userList[i] = userList[i+1];		
+		}
+		userCount--;
+	}
+	
+	// 체크아웃
+	public void checkOut(int index) throws Exception{
+		try{
+			addStock(index);// 상품 재고 다시 추가하기
+			int money = userList[index].pay();// 금액 반환받기
+			subUser(index); // userList에서 삭제, 배열 정리하기
+			revenue += money; // 매출에 추가하기
+		}
+		catch(Exception e) {
+			throw new Exception ("잘못된 방법의 체크아웃입니다.");
+		}
+	}
+	
+	/* 매출 setter 함수 */
+	public void setRenvenue(int revenue) {
+		this.revenue = revenue;
+	}
+	
+	// 매출 반환
+	public int getRevenue() {
+		return revenue;
+	}
+	
+	/* fis getter 함수 */
+	public FileInputStream getFis() {
+		return fis;
+	}
+	
+	/* dis getter 함수 */
+	public DataInputStream getDis() {
+		return dis;
+	}
+	
+	
+	/* 파일 생성 함수 */
+	public void makeFile(DataOutputStream dos) throws Exception{
+		try {
+			dos.writeInt(productCount);
+			for (int i = 0; i < productCount; i++) { // product 정보 write
+				productList[i].saveProduct(dos); // read(~inputStream)할 때 고려해야함, productCount와 userCount를 먼저 출력
+			}
+			dos.writeInt(userCount);
+			for (int i = 0; i < userCount; i++) { // user 정보 write
+				userList[i].saveUser(dos);
+			}
+			dos.writeInt(revenue); // 매출액 write
+		}
+		catch (Exception e){
+			throw new Exception ("입출력 오류");
+		}
+	}
+}
---
+++ Assignment5/src/User.java
@@ -0,0 +1,138 @@
+import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+
+public class User {
+	private String name; // 이름
+	private String phone; // 전화번호
+	private String rentalDay; // 대여 날짜
+	private String returnDay; // 반납 예정 날짜
+	private String[] codeList = new String[3]; // 대여 물품 코드 배열
+	private int[] payList = new int[3]; // 대여 금액 배열
+	private int rentalCount = 0; // 대여 물품 코드/ 금액 배열 인덱스 카운트
+	Calendar getToday = Calendar.getInstance();
+	
+	// 생성자
+	User(String name, String phone, String rentalDay, String returnDay)
+	{
+		this.name = name;
+		this.phone = phone;
+		this.rentalDay = rentalDay;
+		this.returnDay = returnDay;
+	}
+	
+	// 이름 반환
+	public String getName()
+	{
+		return name;
+	}
+	
+	// 전화번호 반환
+	public String getPhone()
+	{
+		return phone;
+	}
+	
+	// 대여 일자 반환
+	public String getRentalDay()
+	{
+		return rentalDay;
+	}
+	
+	// 반납 예정 일자 반환
+	public String getReturnDay()
+	{
+		return returnDay;
+	}
+	
+	// 대여 물품 코드 배열 인덱스 카운트 반환
+	public int getRentalCount() {
+		return rentalCount;
+	}
+	
+	// 대여 물품 코드/ 금액 배열에 코드 추가
+	public void addProduct(String code, int money)
+	{
+		codeList[rentalCount] = code;
+		payList[rentalCount] = money;
+		rentalCount++;
+	}
+	
+	// 대여 물품 코드 배열 i번째 리턴
+	public String codeAt(int i)
+	{
+		// 대여 물품 코드 배열 i번째 상품 객체 return
+		return codeList[i];
+	}
+	
+	// 대여 금액 배열 i번째 리턴
+	public int payAt(int i)
+	{
+		// 대여 금액 배열 i번째 상품 객체 return
+		return payList[i];
+	}
+	
+	// 결제 함수
+	public int pay() throws Exception
+	{
+		// 물건 금액 합 계산하기
+		int money = 0;
+		for(int i = 0; i < rentalCount; i++)
+		{
+			money += payList[i];
+		}
+		
+		// 반납 일자 입력하기
+		getToday.setTime(new Date());
+		
+		// 대여 일자 불러오기
+		Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(rentalDay);
+		Calendar rentalDate = Calendar.getInstance();
+		rentalDate.setTime(date1);
+		
+		// 반납 예정 일자 불러오기
+		Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse(returnDay);
+		Calendar returnDate = Calendar.getInstance();
+		returnDate.setTime(date2);
+		
+		// 대여하기로 한 기간 계산하기 (반납 예정 일자 - 대여 일자)
+		// 밀리초를 차이 구한 후 일 수로 변환
+		int day1 = (int) ((returnDate.getTimeInMillis() - rentalDate.getTimeInMillis()) / (1000 * 60 * 60 * 24));
+
+		// 대여 기간 계산하기 (반납 일자 - 대여 일자)
+		// 밀리초를 차이 구한 후 일 수로 변환
+		int day2 = (int) ((getToday.getTimeInMillis() - rentalDate.getTimeInMillis()) / (1000 * 60 * 60 * 24));
+		
+		/*금액 관련 규칙 설정
+		 * 날짜 지켜서 반납 => 상품 금액 총 합 x 대여 기간
+		 * 먼저 반납 => 상품 금액 총 합 x 실제 대여 기간(남은 기간은 가격 책정 x)
+		 * 나중에 반납 => 상품 금액 총 합 x 실제 대여 기간 + 연체료(하루 대여료)*/
+		
+		// 총 금액 계산하기
+		if (day1 == day2) // 반납 예정 날짜에 반납
+			return money *= (day1 + 1); // 당일 대여 반납도 1일로 처리
+		
+		else if (day1 > day2)// 반납 예정 날짜보다 빨리 반납
+			return money *= (day2 + 1); // 당일 대여 반납도 1일로 처리
+		
+		else // 반납 예정 날짜보다 늦게 반납
+			return money *= (day2 + 2); // 연체료 (하루 대여료) 추가하여서 반환
+	}
+	
+	/* user 정보 write 함수 */
+	public void saveUser(DataOutputStream dos) throws Exception {
+		try {
+			dos.writeUTF(name); // user 이름 write
+			dos.writeUTF(phone); // user 전화번호 write
+			dos.writeUTF(rentalDay); // user 대여일자 write
+			dos.writeUTF(returnDay); // user 반납일자 write
+			dos.writeInt(rentalCount); // 대여 물품 코드 배열 개수 write
+			for (int i = 0; i < rentalCount; i++) {
+				dos.writeUTF(codeAt(i)); // 대여 물품 코드 write
+			}
+		}
+		catch (Exception e){
+			throw new Exception ("입출력 오류");
+		}
+	}
+}
